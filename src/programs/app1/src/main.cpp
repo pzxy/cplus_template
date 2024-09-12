c123177cc6c1aa5319557543342958b0fe0347f3
kwin <wonkung@163.com>
2024-09-12 14:12:07 +0800

    init version

---
+++ src/programs/app1/src/main.cpp
@@ -0,0 +1,9 @@
+#include <httplib.h>
+#include <iostream>
+
+using namespace std;
+int main() {
+    httplib::Client cli("https://time.geekbang.org");
+    auto res = cli.Get("/resource?m=0&d=3&c=3");
+    cout << res->body << endl;
+}
