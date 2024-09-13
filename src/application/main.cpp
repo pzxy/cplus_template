e2aab2dfd6ecd27a743c01cfc3bbce945625c446
kwin <wonkung@163.com>
2024-09-13 14:29:12 +0800

    feat: add log

--- src/application/main.cpp
+++ src/application/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <spdlog/spdlog.h>
 #include "examples/demo1/box.h"
 
 using namespace std;
 
 int main() {
+    spdlog::info("Welcome to spdlog!");
     xxx::examples::demo1::Box box{};
     box.set(1,2.3,5);
     cout << "this box size is : "<< box.get() << endl;
